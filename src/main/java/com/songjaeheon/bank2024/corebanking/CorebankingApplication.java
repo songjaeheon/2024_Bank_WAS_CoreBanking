79949282facd2b28747183b058e47b1b26d3b7c9
songjaeheon <spes2you@gmail.com>
2024-07-02 13:50:19 +0900

    feat: initialize spring boot project

---
+++ src/main/java/com/songjaeheon/bank2024/corebanking/CorebankingApplication.java
@@ -0,0 +1,13 @@
+package com.songjaeheon.bank2024.corebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorebankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/songjaeheon/bank2024/corebanking/CorebankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songjaeheon.bank2024.corebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorebankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
